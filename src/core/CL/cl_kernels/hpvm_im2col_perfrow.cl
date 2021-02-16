be45654167a23806f15a38a78b828d324833536b
Matevž Fabjančič <matevz.fabjancic@gmail.com>
2021-02-16 10:17:11 +0100

    Restart hpvm_im2col_perfrow

--- src/core/CL/cl_kernels/hpvm_im2col_perfrow.cl
+++ src/core/CL/cl_kernels/hpvm_im2col_perfrow.cl
@@ -1,7 +1,42 @@
+/*
+ * Copyright (c) 2018-2020 Arm Limited.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include "helpers.h"
 
 #if defined(DATA_TYPE) && defined(ELEMENT_SIZE)
+
+#if ELEMENT_SIZE == 1
+#define COND_DATA_TYPE char
+#elif ELEMENT_SIZE == 2
+#define COND_DATA_TYPE short
+#elif ELEMENT_SIZE == 4
+#define COND_DATA_TYPE int
+#else // ELEMENT_SIZE
+#error "Element size not support"
+#endif // ELEMENT_SIZE
+
 #if defined(CONVOLVED_WIDTH) && defined(SRC_WIDTH) && defined(SRC_HEIGHT) && defined(STRIDE_X) && defined(STRIDE_Y) && defined(SRC_DEPTH) && defined(PAD_LEFT) && defined(PAD_RIGHT) && defined(PAD_TOP) && defined(PAD_BOTTOM) && defined(PAD_VALUE)
+#if defined(DILATION_X) && defined(DILATION_Y)
 /** This opencl kernel performs a generic im2col implementation when the data layout is NCHW
  *
  * @note The data type must be passed at compile time using -DDATA_TYPE: e.g. -DDATA_TYPE=float
@@ -11,6 +46,7 @@
  * @note The pad_left, pad_right, pad_top and pad_bottom must be passed at compile time using -DPAD_LEFT, -DPAD_RIGHT, -DPAD_TOP and -DPAD_BOTTOM: e.g. -DPAD_LEFT=1, -DPAD_RIGHT=2, -DPAD_TOP=3 and -DPAD_BOTTOM=2
  * @note The zero value to store in case we load values out-of-bounds must be passed at compile time using -DPAD_VALUE: e.g. -DPAD_VALUE=0.0
  * @note The stride along the X and Y directions must be passed at compile time using -DSTRIDE_X and -DSTRIDE_Y: e.g. -DSTRIDE_X=1 and -DSTRIDE_Y=1
+ * @note The dilation_x and dilation_y must be passed at compile time using -DDILATION_X and -DDILATION_Y: e.g. -DDILATION_X=1, -DDILATION_Y=1
  * @note In case biases will be added to the convolution -DHAS_BIAS has to be passed to append the final matrix with 1 in each row.
  * @note In case grouping is performed, the number of groups must be passed at compile time using -DNUM_GROUPS: e.g. -DNUM_GROUPS=4
  *
@@ -32,16 +68,16 @@
  * @param[in]  dst_offset_first_element_in_bytes The offset of the first element in the destination tensor
  * @param[in]  src_stride_w                      Stride of the source tensor in W dimension (in bytes).
  * @param[in]  dst_stride_w                      Stride of the destination tensor in W dimension (in bytes).
- * @param[in]  perfrow                           Every [perfrow]-th row will be skipped.
  */
-__kernel void hpvm_im2col_perfrow_generic_nchw(
+__kernel void im2col_generic_nchw(
     TENSOR3D_DECLARATION(src),
+#if defined(NUM_GROUPS)
+    TENSOR3D_DECLARATION(dst),
+#else  // defined(NUM_GROUPS)
     IMAGE_DECLARATION(dst),
-    uint src_stride_w, // w means dim[3] (therefore N), not Width
-    uint dst_stride_w, // w means dim[3] (therefore N), not Width
-    uint perfrow_start,
-    uint perfrow_every,
-    uint perfrow_h_eff)
+#endif // defined(NUM_GROUPS)
+    uint src_stride_w,
+    uint dst_stride_w)
 {
     const int xc    = get_global_id(0);             // x coordinate in the convolved tensor
     const int yc    = get_global_id(1);             // y coordinate in the convolved tensor
@@ -50,22 +86,34 @@ __kernel void hpvm_im2col_perfrow_generic_nchw(
 
     // Calculate input indices
     const int xi = xc * STRIDE_X - PAD_LEFT;
-    const int yi = yc * perfrow_every * STRIDE_Y - PAD_TOP;
+    const int yi = yc * STRIDE_Y - PAD_TOP;
 
     // Calculate output indices
-    const int xo = ch * KERNEL_WIDTH * KERNEL_HEIGHT;
+#if defined(NUM_GROUPS)
+    const int xo = (ch % (SRC_DEPTH / NUM_GROUPS)) * KERNEL_WIDTH * KERNEL_HEIGHT;
+    const int zo = ch / (SRC_DEPTH / NUM_GROUPS);
+#else                                         // defined(NUM_GROUPS)
+    const int xo                   = ch * KERNEL_WIDTH * KERNEL_HEIGHT;
+#endif                                        // defined(NUM_GROUPS)
     const int yo = xc + yc * CONVOLVED_WIDTH; // Index of the convolution
 
-    __global uchar *input_ptr      = src_ptr + src_offset_first_element_in_bytes + ch * src_stride_z + batch * src_stride_w;
+    __global uchar *input_ptr = src_ptr + src_offset_first_element_in_bytes + ch * src_stride_z + batch * src_stride_w;
+#if defined(NUM_GROUPS)
+    __global DATA_TYPE *output_ptr = ((__global DATA_TYPE *)(dst_ptr + dst_offset_first_element_in_bytes + yo * dst_stride_y + zo * dst_stride_z + batch * dst_stride_w)) + xo;
+#else  // defined(NUM_GROUPS)
     __global DATA_TYPE *output_ptr = ((__global DATA_TYPE *)(dst_ptr + dst_offset_first_element_in_bytes + yo * dst_stride_y + batch * dst_stride_w)) + xo;
+#endif // defined(NUM_GROUPS)
 
     // Linearize convolution elements
     for(int yk = 0; yk < KERNEL_HEIGHT; ++yk)
     {
-        int y = yi + yk;
+        int y = yi + yk * DILATION_Y;
         for(int xk = 0; xk < KERNEL_WIDTH; ++xk, ++output_ptr)
         {
-            int x = xi + xk;
+            int x = xi + xk * DILATION_X;
+#if PAD_LEFT == 0 && PAD_TOP == 0 && PAD_RIGHT == 0 && PAD_BOTTOM == 0
+            *output_ptr = *((__global DATA_TYPE *)(input_ptr + x * src_stride_x + y * src_stride_y));
+#else  // PAD_LEFT == 0 && PAD_TOP == 0 && PAD_RIGHT == 0 && PAD_BOTTOM == 0
             if(x < 0 || x >= SRC_WIDTH || y < 0 || y >= SRC_HEIGHT)
             {
                 *output_ptr = PAD_VALUE;
@@ -74,8 +122,22 @@ __kernel void hpvm_im2col_perfrow_generic_nchw(
             {
                 *output_ptr = *((__global DATA_TYPE *)(input_ptr + x * src_stride_x + y * src_stride_y));
             }
+#endif // PAD_LEFT == 0 && PAD_TOP == 0 && PAD_RIGHT == 0 && PAD_BOTTOM == 0
         }
     }
+
+#ifdef HAS_BIAS
+#if defined(NUM_GROUPS)
+    if((xo / (KERNEL_WIDTH * KERNEL_HEIGHT)) == (SRC_DEPTH / NUM_GROUPS - 1))
+#else  // defined(NUM_GROUPS)
+    if(ch == (SRC_DEPTH - 1))
+#endif // defined(NUM_GROUPS)
+    {
+        *output_ptr = 1.0f;
+    }
+#endif // HAS_BIAS
 }
-#endif
-#endif
+
+#endif // defined(DILATION_X) && defined(DILATION_Y)
+#endif // defined(CONVOLVED_WIDTH) && defined(SRC_WIDTH) && defined(SRC_HEIGHT) && defined(STRIDE_X) && defined(STRIDE_Y) && defined(KERNEL_WIDTH) && defined(KERNEL_HEIGHT) && defined(SRC_DEPTH) && defined(PAD_LEFT) && defined(PAD_RIGHT) && defined(PAD_TOP) && defined(PAD_BOTTOM) && defined(PAD_VALUE) && defined(VECTOR_SIZE) && defined(BOUNDARY_VECTOR_SIZE)
+#endif // defined(DATA_TYPE) && defined(ELEMENT_SIZE)
--- src/core/CL/kernels/HPVMIm2ColPerfRowKernel.h
+++ src/core/CL/kernels/HPVMIm2ColPerfRowKernel.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020 Arm Limited.
+ * Copyright (c) 2017-2020 Arm Limited.
  *
  * SPDX-License-Identifier: MIT
  *
@@ -21,162 +21,116 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-#ifndef ARM_COMPUTE_CLACCUMULATEKERNEL_H
-#define ARM_COMPUTE_CLACCUMULATEKERNEL_H
+#ifndef ARM_COMPUTE_HPVMIM2COLPERFROWKERNEL_H
+#define ARM_COMPUTE_HPVMIM2COLPERFROWKERNEL_H
 
-#include "arm_compute/core/CL/CLCompileContext.h"
-#include "arm_compute/core/CL/CLHelpers.h"
-#include "arm_compute/core/CL/CLKernelLibrary.h"
-#include "arm_compute/core/Error.h"
-#include "arm_compute/core/Types.h"
-#include "arm_compute/core/Utils.h"
+#include "arm_compute/core/Size2D.h"
 #include "src/core/CL/ICLKernel.h"
-#include "src/core/CL/ICLSimple2DKernel.h"
-#include "src/core/CL/ICLSimpleKernel.h"
-#include "src/core/helpers/WindowHelpers.h"
-#include "support/Cast.h"
-#include "support/StringSupport.h"
-
-#include <cstddef>
-#include <cstdint>
 
 namespace arm_compute
 {
 class ICLTensor;
 
-/** Interface for the accumulate kernel.
+/** Interface for the im2col reshape kernel.
  *
- * Accumulation is computed by:
- * @f[ accum(x,y) = accum(x,y) + input(x+offset_x,y+offset_y) @f]
+ * Rearranges image blocks into columns. It is used to strip out each convolution block to a single column.
+ * It is used to transform a convolution to a plain matrix multiplication.
+ *
+ * For example taking into account the image below and assuming 3x3 image blocks with stride of 1 we have:
+ * @f[
+ * \left( \begin{array}{cccc}
+ * a00 & a01 & a02 & a03 \\
+ * a10 & a11 & a12 & a13 \\
+ * a20 & a21 & a22 & a23 \\
+ * a30 & a31 & a32 & a33 \\
+ * \end{array} \right)
+ * =
+ * \left( \begin{array}{ccccccccc}
+ * a00 & a01 & a02 & a10 & a11 & a12 & a20 & a21 & a22 \\
+ * a01 & a02 & a03 & a11 & a12 & a13 & a21 & a22 & a23 \\
+ * a10 & a11 & a12 & a20 & a21 & a22 & a30 & a31 & a32 \\
+ * a11 & a12 & a13 & a21 & a22 & a23 & a31 & a32 & a33 \\
+ * \end{array} \right)
+ * @f]
  */
 class HPVMIm2ColPerfRowKernel : public ICLKernel
 {
 public:
-    /** Set the input and accumulation tensors.
+    /** Default constructor */
+    HPVMIm2ColPerfRowKernel();
+    /** Prevent instances of this class from being copied (As this class contains pointers) */
+    HPVMIm2ColPerfRowKernel(const HPVMIm2ColPerfRowKernel &) = delete;
+    /** Prevent instances of this class from being copied (As this class contains pointers) */
+    HPVMIm2ColPerfRowKernel &operator=(const HPVMIm2ColPerfRowKernel &) = delete;
+    /** Allow instances of this class to be moved */
+    HPVMIm2ColPerfRowKernel(HPVMIm2ColPerfRowKernel &&) = default;
+    /** Allow instances of this class to be moved */
+    HPVMIm2ColPerfRowKernel &operator=(HPVMIm2ColPerfRowKernel &&) = default;
+    /** Set the input and output of the kernel.
      *
-     * @param[in]  input Source tensor. Data types supported: U8.
-     * @param[out] accum Destination tensor. Data types supported: S16.
+     * @param[in]  input       The input tensor to convert. 3 lower dimensions represent a single input [width, height, IFM],
+     *                         while every optional dimension from 4 and above represent a batch of inputs. Data types supported: QASYMM8/QASYMM8_SIGNED/F16/F32
+     * @param[out] output      The output tensor. First 2 lower dimensions represent a transform of each 3D input,
+     *                         while every dimension above represents a batch. Data types supported: Same as @p input
+     * @param[in]  kernel_dims The kernel dimensions (width and height).
+     * @param[in]  conv_info   Contains padding and stride information described in @ref PadStrideInfo.
+     * @param[in]  has_bias    In case biases are provided expands the matrix with 1.
+     *                         This is valid only for non-quantized inputs.
+     * @param[in]  dilation    (Optional) Dilation, in elements, across x and y. Defaults to (1, 1).
+     * @param[in]  num_groups  (Optional) Number of groups when performing a grouped convolution.
+     *                         Number of groups other than 1 is only supported for NCHW data layout.
+     *                         Number of groups should be multiple to the number of channels.
      */
-    void configure(const ICLTensor *input, ICLTensor *output,
-                   const Size2D &kernel_dims, const PadStrideInfo &conv_info,
-                   size_t perfrow_start, size_t perfrow_every, size_t perfrow_h_eff)
-    {
-        configure(CLKernelLibrary::get().get_compile_context(), input, output, kernel_dims, conv_info, perfrow_start, perfrow_every, perfrow_h_eff);
-    }
-
-    /** Set the input and accumulation tensors.
+    void configure(const ICLTensor *input, ICLTensor *output, const Size2D &kernel_dims, const PadStrideInfo &conv_info, bool has_bias, const Size2D &dilation = Size2D(1U, 1U),
+                   unsigned int num_groups = 1);
+    /** Set the input and output of the kernel.
      *
      * @param[in]  compile_context The compile context to be used.
-     * @param[in]  input           Source tensor. Data types supported: U8.
-     * @param[out] accum           Destination tensor. Data types supported: S16.
+     * @param[in]  input           The input tensor to convert. 3 lower dimensions represent a single input [width, height, IFM],
+     *                             while every optional dimension from 4 and above represent a batch of inputs. Data types supported: QASYMM8/QASYMM8_SIGNED/F16/F32
+     * @param[out] output          The output tensor. First 2 lower dimensions represent a transform of each 3D input,
+     *                             while every dimension above represents a batch. Data types supported: Same as @p input
+     * @param[in]  kernel_dims     The kernel dimensions (width and height).
+     * @param[in]  conv_info       Contains padding and stride information described in @ref PadStrideInfo.
+     * @param[in]  has_bias        In case biases are provided expands the matrix with 1.
+     * @param[in]  dilation        (Optional) Dilation, in elements, across x and y. Defaults to (1, 1).
+     * @param[in]  num_groups      (Optional) Number of groups when performing a grouped convolution. num_groups != 1 is only supported for NCHW data layout
      */
-    void configure(const CLCompileContext &compile_context,
-                   const ICLTensor *input, ICLTensor *output,
-                   const Size2D &kernel_dims, const PadStrideInfo &conv_info,
-                   size_t perfrow_start, size_t perfrow_every, size_t perfrow_h_eff)
-    {
-        _input  = input;
-        _output = output;
+    void configure(const CLCompileContext &compile_context, const ICLTensor *input, ICLTensor *output, const Size2D &kernel_dims, const PadStrideInfo &conv_info, bool has_bias,
+                   const Size2D &dilation   = Size2D(1U, 1U),
+                   unsigned int  num_groups = 1);
+    /** Static function to check if given info will lead to a valid configuration of @ref HPVMIm2ColPerfRowKernel
+     *
+     * @param[in] input       The input tensor to convert. 3 lower dimensions represent a single input [width, height, IFM],
+     *                        while every optional dimension from 4 and above represent a batch of inputs. Data types supported: QASYMM8/QASYMM8_SIGNED/F16/F32
+     * @param[in] output      The output tensor. First 2 lower dimensions represent a transform of each 3D input,
+     *                        while every dimension above represents a batch. Data types supported: Same as @p input
+     * @param[in] kernel_dims The kernel dimensions (width and height).
+     * @param[in] conv_info   Contains padding and stride information described in @ref PadStrideInfo.
+     * @param[in] has_bias    In case biases are provided expands the matrix with 1.
+     *                        This is valid only for non-quantized inputs.
+     * @param[in] dilation    (Optional) Dilation, in elements, across x and y. Defaults to (1, 1).
+     * @param[in] num_groups  (Optional) Number of groups when performing a grouped convolution.
+     *                        Number of groups other than 1 is only supported for NCHW data layout.
+     *                        Number of groups should be multiple to the number of channels.
+     *
+     * @return a status
+     */
+    static Status validate(const ITensorInfo *input, const ITensorInfo *output, const Size2D &kernel_dims, const PadStrideInfo &conv_info, bool has_bias, const Size2D &dilation = Size2D(1U, 1U),
+                           unsigned int num_groups = 1);
 
-        auto input_info  = input->info();
-        auto output_info = output->info();
-        auto data_type   = input_info->data_type();
+    // Inherited methods overridden:
+    void run(const Window &window, cl::CommandQueue &queue) override;
 
-        _n = input_info->dimension(DataLayoutDimension::BATCHES);
-        _c = input_info->dimension(DataLayoutDimension::CHANNEL);
-        _h = input_info->dimension(DataLayoutDimension::HEIGHT);
-        _w = input_info->dimension(DataLayoutDimension::WIDTH);
-
-        _kh = kernel_dims.height;
-        _kw = kernel_dims.width;
-
-        _perfrow_start = perfrow_start;
-        _perfrow_every = perfrow_every;
-        _perfrow_h_eff = perfrow_h_eff;
-
-        auto stride_x = conv_info.stride().first;
-        auto stride_y = conv_info.stride().second;
-
-        size_t out_w = (_w + stride_x - 1) / stride_x;
-        size_t out_h = (_h + stride_y - 1) / stride_y;
-        out_h        = (out_h + perfrow_every - 1) / perfrow_every;
-
-        size_t convolved_height = _kh * _kw;
-        size_t convolved_width  = _n * _c * out_h * out_w;
-
-        ARM_COMPUTE_ERROR_ON(output_info->dimension(DataLayoutDimension::HEIGHT) != convolved_height);
-        ARM_COMPUTE_ERROR_ON(output_info->dimension(DataLayoutDimension::WIDTH) != convolved_width);
-
-        CLBuildOptions opts{};
-        opts.add_option("-DDATA_TYPE=" + get_cl_type_from_data_type(data_type));
-        opts.add_option("-DELEMENT_SIZE=" + support::cpp11::to_string(input_info->element_size()));
-        opts.add_option("-DKERNEL_WIDTH=" + support::cpp11::to_string(kernel_dims.width));
-        opts.add_option("-DKERNEL_HEIGHT=" + support::cpp11::to_string(kernel_dims.height));
-        opts.add_option("-DCONVOLVED_HEIGHT=" + support::cpp11::to_string(convolved_height));
-        opts.add_option("-DCONVOLVED_WIDTH=" + support::cpp11::to_string(convolved_width));
-        opts.add_option("-DSTRIDE_X=" + support::cpp11::to_string(conv_info.stride().first));
-        opts.add_option("-DSTRIDE_Y=" + support::cpp11::to_string(conv_info.stride().second));
-        opts.add_option("-DPAD_LEFT=" + support::cpp11::to_string(conv_info.pad_left()));
-        opts.add_option("-DPAD_TOP=" + support::cpp11::to_string(conv_info.pad_top()));
-        opts.add_option("-DPAD_RIGHT=" + support::cpp11::to_string(conv_info.pad_right()));
-        opts.add_option("-DPAD_BOTTOM=" + support::cpp11::to_string(conv_info.pad_bottom()));
-        opts.add_option("-DSRC_WIDTH=" + support::cpp11::to_string(_w));
-        opts.add_option("-DSRC_HEIGHT=" + support::cpp11::to_string(_h));
-        opts.add_option("-DSRC_DEPTH=" + support::cpp11::to_string(_c));
-        opts.add_option("-DPAD_VALUE=0");
-
-        _kernel = create_kernel(compile_context, "hpvm_im2col_perfrow_generic_nchw", opts.options());
-
-        unsigned int idx = num_arguments_per_3D_tensor() + num_arguments_per_2D_tensor();
-        _kernel.setArg(idx++, (unsigned int)input_info->strides_in_bytes()[3]);
-        _kernel.setArg(idx++, (unsigned int)output_info->strides_in_bytes()[3]);
-        _kernel.setArg(idx++, (unsigned int)_perfrow_start);
-        _kernel.setArg(idx++, (unsigned int)_perfrow_every);
-        _kernel.setArg(idx++, (unsigned int)_perfrow_h_eff);
-
-        Window win = calculate_max_window(*output_info);
-
-        // TODO: tune local size
-        ICLKernel::configure_internal(win);
-        // ICLKernel::configure_internal(win, cl::NDRange(16u, 1u));
-
-        std::ostringstream config_id_str;
-        config_id_str << "hpvm_im2col_perfrow_generic_nchw" << string_from_data_type(data_type) << "_"
-                      << _w << "_" << _h << "_" << _c << "_" << _n;
-
-        _config_id = config_id_str.str();
-    }
-
-    void run(const Window &window, cl::CommandQueue &queue) override
-    {
-        ITensorPack pack{};
-        pack.add_tensor(TensorType::ACL_SRC, _input);
-        pack.add_tensor(TensorType::ACL_DST, _output);
-        run_op(pack, window, queue);
-    }
-
-    void run_op(ITensorPack &tensors, const Window &window, cl::CommandQueue &queue) override
-
-    {
-        Window slice = window.first_slice_window_2D();
-
-        const auto input  = utils::cast::polymorphic_downcast<const ICLTensor *>(tensors.get_const_tensor(TensorType::ACL_SRC));
-        auto       output = utils::cast::polymorphic_downcast<ICLTensor *>(tensors.get_tensor(TensorType::ACL_DST));
-
-        // Set inputs
-        unsigned int idx = 0;
-        add_2D_tensor_argument(idx, input, slice);
-        add_2D_tensor_argument(idx, output, slice);
-        enqueue(queue, *this, slice, lws_hint());
-    }
-
-private:
-    const ICLTensor *_input;
-    const ICLTensor *_output;
-
-    size_t _n, _c, _h, _w;
-    size_t _kh, _kw;
-    size_t _perfrow_start, _perfrow_every, _perfrow_h_eff;
+public:
+    const ICLTensor *                     _input;
+    ICLTensor *                           _output;
+    DataLayout                            _data_layout;
+    std::pair<unsigned int, unsigned int> _convolved_dims;
+    unsigned int                          _num_elems_processed_per_iteration;
+    Size2D                                _kernel_dims;
+    PadStrideInfo                         _conv_info;
+    unsigned int                          _num_groups;
 };
 } // namespace arm_compute
-#endif /*ARM_COMPUTE_CLACCUMULATEKERNEL_H */
+#endif /*ARM_COMPUTE_HPVMIM2COLPERFROWKERNEL_H */
